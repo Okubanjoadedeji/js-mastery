3623256f0d69bb066e1776f4eb0166f3c2e49ec2
Okubanjoadedeji <okubanjoadedeji39@gmail.com>
2023-11-09 15:38:07 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,196 @@
+/*
+// Introduction
+let intro = 'Introduction to Variables';
+console.log(intro);
+
+// Strings 
+const nameInput = 'deji';
+const backTick = `hello there ${nameInput} No ${2+2}! `;
+const doubleQuotes = "hello world!";
+const singleQuotes = 'hello kids!';
+
+console.log(backTick);
+console.log(doubleQuotes);
+console.log(singleQuotes);
+
+
+// Numbers 
+const wholeNumber = 5;
+const decimalNumber = 0.5;
+
+console.log(wholeNumber);
+console.log(decimalNumber);
+
+
+let firstNumber = 8;
+let secondNumber = 16;
+let result = firstNumber + secondNumber;
+console.log(firstNumber);
+console.log(secondNumber);
+console.log(result);
+
+
+
+// Booleans
+const ifCool = true;
+
+console.log(ifCool);
+
+if (ifCool) {
+  console.log("Hi man, you're cool");
+} else {
+  console.log('Oh, hi .....');
+}
+
+
+// Null 
+let age = null;
+
+console.log(age);
+
+undefined 
+let y;
+
+console.log(y);
+
+
+
+// Objects
+const person = {
+  name: 'deji',
+  age: 19,
+}
+
+console.log(person);
+
+// Dot notation
+console.log(person.name, person.age);
+
+// Array 
+const arr = [1, 2, 3, 4]
+
+console.log(arr);
+
+const date = new Date()
+
+console.log(date);
+*/
+
+
+
+
+
+
+// Comparison Operators => true\false
+// const a = 5;
+// const b = 10;
+
+// Greater than
+// console.log(a > b);
+
+// Greater than or Equal to
+// console.log(a >= b);
+
+// Less than
+// console.log(a < b);
+
+// Less than or Equal to
+// console.log(a <= b);
+
+// Equal to 
+// console.log(a == b);
+
+// Not Equal to 
+// console.log(a != b);
+
+// Strict equality
+// console.log(a === b);
+
+// Strict inequality
+// console.log(a !== b);
+
+// Logical Operators
+// AND && => ALL OPERANDS ARE TRUE => TRUE
+// console.log(true && true); //true
+// console.log(true && false); //false
+// console.log(false && false); //false
+// console.log(true && false && true); //false
+// console.log(true && true && true); //true
+
+// OR ||   => ATLEAST ONE OPERAND IS TRUE => TRUE
+// console.log(true || true); //true
+// console.log(true || false); //true
+// console.log(false || false); //false
+// console.log(false || true); //true
+
+// NOT ! => OPPOSITE OF ORIGINAL OPERAND TRUE => FALSE, FALSE => TRUE
+// console.log(!true); //false
+// console.log(!false); //ture
+
+// Statement 
+/*
+const age = 16;
+
+if (age > 18) {
+  console.log("You're welcome!");
+} else if (age === 18) {
+  console.log("You just turned 18!");
+} else{
+  console.log("Get out!");
+}
+*/
+
+
+// Logic and Control Flow
+
+// The "while" Loop
+// let i = 0;
+
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// The "for" Loop
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+
+
+
+// FUNCTIONS
+// A BLOCK OF CODE => PERFORMS A TASK
+
+// //A FUNCTION DECLARATION (declaring a function)
+// function square(number) {
+//   return number * number
+// }
+
+// //A FUNCTION CALL (calling/executing/invoking a function)
+// const result = square(4)
+// console.log(result);
+
+// A function declaration
+function name(params) {
+  // Statements
+  // have access to 'this' keyword
+}
+
+// A function expression
+const name = function(params) {
+  //statements
+}
+
+// An arrow function
+const name = (params) => {
+  // statements
+  // used 99% of the time
+}
+
+
+
+
+
+
